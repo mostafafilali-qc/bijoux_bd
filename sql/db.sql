e663c49ac441f14b8ca0f5baabc9c64cc7bb34d5
kingmufasa99 <77929993+kingmufasa99@users.noreply.github.com>
2023-04-07 02:20:41 -0400

    feat: utiliser db

--- sql/db.sql
+++ sql/db.sql
@@ -1,3 +1,7 @@
+create database bijoux;
+use bijoux;
+show tables;
+
 CREATE TABLE Clients (
   ID_Client INT,
   Nom VARCHAR(50),
@@ -54,16 +58,16 @@ CREATE TABLE Images (
   FOREIGN KEY (ID_Produit) REFERENCES Produits(ID_Produit)
 );
 
-INSERT INTO Produits (ID_Produit, Nom_du_produit, Description, Prix, Stock_disponible, ID_Categorie)
-VALUES 
-(1, 'Bague en or blanc et diamant', 'Bague en or blanc 18 carats sertie d''un diamant de qualité supérieure.', 1499.99, 10, 1),
-(2, 'Collier en argent et perles de culture', 'Collier en argent sterling 925 avec des perles de culture d''eau douce. Parfait pour un style décontracté ou pour les occasions spéciales.', 299.99, 20, 2),
-(3, 'Bracelet en argent et topaze bleue', 'Bracelet en argent sterling 925 serti de topazes bleues de qualité AAA. Un bijou élégant pour toutes les occasions.', 599.99, 15, 1),
-(4, 'Boucles d''oreilles en or blanc et diamant', 'Boucles d''oreilles en or blanc 18 carats serties de diamants de qualité supérieure. Un accessoire étincelant pour une soirée élégante.', 1899.99, 5, 1),
-(5, 'Bracelet jonc en argent et zircon', 'Bracelet jonc en argent sterling 925 serti de zircons cubiques. Un bijou simple mais élégant qui s''adapte à tous les styles.', 149.99, 25, 2),
-(6, 'Collier en or rose et pierres précieuses', 'Collier en or rose 18 carats serti de pierres précieuses de différentes couleurs. Un bijou unique pour une occasion spéciale.', 899.99, 8, 1),
-(7, 'Bague en argent et pierre de lune', 'Bague en argent sterling 925 sertie d''une pierre de lune naturelle. Un bijou délicat et romantique.', 399.99, 12, 1),
-(8, 'Boucles d''oreilles en or jaune et rubis', 'Boucles d''oreilles en or jaune 18 carats serties de rubis de qualité supérieure. Un bijou précieux pour une soirée inoubliable.', 2599.99, 3, 1),
-(9, 'Collier en argent et cristal de Swarovski', 'Collier en argent sterling 925 serti de cristaux de Swarovski de qualité supérieure. Un bijou étincelant pour une soirée élégante.', 499.99, 18, 2),
-(10, 'Bracelet en or jaune et perles de culture', 'Bracelet en or jaune 18 carats avec des perles de culture d''eau douce. Un bijou classique pour toutes les occasions.', 799.99, 10, 1),
-(11, 'Bague en argent et améthyste', 'Bague en argent sterling 925 sertie d''une améthyste de qualité AAA. Un bijou élégant pour une soirée habillée.', 499.99, 7, 1)
+-- INSERT INTO Produits (ID_Produit, NomProduit, Description, Prix, Stockdisponible, ID_Categorie)
+-- VALUES 
+-- (1, 'Bague en or blanc et diamant', 'Bague en or blanc 18 carats sertie d''un diamant de qualité supérieure.', 1499.99, 10, 1),
+-- (2, 'Collier en argent et perles de culture', 'Collier en argent sterling 925 avec des perles de culture d''eau douce. Parfait pour un style décontracté ou pour les occasions spéciales.', 299.99, 20, 2),
+-- (3, 'Bracelet en argent et topaze bleue', 'Bracelet en argent sterling 925 serti de topazes bleues de qualité AAA. Un bijou élégant pour toutes les occasions.', 599.99, 15, 1),
+-- (4, 'Boucles d''oreilles en or blanc et diamant', 'Boucles d''oreilles en or blanc 18 carats serties de diamants de qualité supérieure. Un accessoire étincelant pour une soirée élégante.', 1899.99, 5, 1),
+-- (5, 'Bracelet jonc en argent et zircon', 'Bracelet jonc en argent sterling 925 serti de zircons cubiques. Un bijou simple mais élégant qui s''adapte à tous les styles.', 149.99, 25, 2),
+-- (6, 'Collier en or rose et pierres précieuses', 'Collier en or rose 18 carats serti de pierres précieuses de différentes couleurs. Un bijou unique pour une occasion spéciale.', 899.99, 8, 1),
+-- (7, 'Bague en argent et pierre de lune', 'Bague en argent sterling 925 sertie d''une pierre de lune naturelle. Un bijou délicat et romantique.', 399.99, 12, 1),
+-- (8, 'Boucles d''oreilles en or jaune et rubis', 'Boucles d''oreilles en or jaune 18 carats serties de rubis de qualité supérieure. Un bijou précieux pour une soirée inoubliable.', 2599.99, 3, 1),
+-- (9, 'Collier en argent et cristal de Swarovski', 'Collier en argent sterling 925 serti de cristaux de Swarovski de qualité supérieure. Un bijou étincelant pour une soirée élégante.', 499.99, 18, 2),
+-- (10, 'Bracelet en or jaune et perles de culture', 'Bracelet en or jaune 18 carats avec des perles de culture d''eau douce. Un bijou classique pour toutes les occasions.', 799.99, 10, 1),
+-- (11, 'Bague en argent et améthyste', 'Bague en argent sterling 925 sertie d''une améthyste de qualité AAA. Un bijou élégant pour une soirée habillée.', 499.99, 7, 1);
